d117335ea0056e11ed78ef6d852bf9175fb2709c
mahfoozalamcse <amahfooz1007@gmail.com>
2025-04-07 17:44:45 +0530

    portfolio mahfooz use javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+function scrollToContact() {
+    document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+  }
+  
+  // Optional: Handle contact form submission
+  document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for your message, Mahfooz will get back to you!");
+  });
+  
