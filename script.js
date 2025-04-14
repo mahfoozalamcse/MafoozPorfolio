b43504839a145fffb67bab324cc5c517604fac47
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-04-14 20:05:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function scrollToContact() {
   }
   
   // Optional: Handle contact form submission
-  document.querySelector("form").addEventListener("submit", function (e) {
+  document.querySelector("form").addEventListener("submit", function (e) { 
     e.preventDefault();
     alert("Thank you for your message, Mahfooz will get back to you!");
   });
